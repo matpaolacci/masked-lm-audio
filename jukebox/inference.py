a1feef8a3c75aaf484040a0922e8084c38378241
Mattia Paolacci <mat.paolacci@gmail.com>
2024-12-29 16:40:46 +0100

    added implementation for inference

--- jukebox/inference.py
+++ jukebox/inference.py
@@ -6,8 +6,24 @@ import jukebox.utils.dist_adapter as dist
 from make_models import load_checkpoint
 from train import evaluate, get_ddp
 from logging import init_logging
+import torch as t
+from utils.audio_utils import save_wav
 
-def run(hps="vqvae", port=29500, **kwargs):
+def inference(model, hps, data_processor, logger):
+    model.eval()
+    with t.no_grad():
+        for i, x in logger.get_range(data_processor.test_loader):
+            x = x.to('cuda', non_blocking=True)
+                
+            forw_kwargs = dict(loss_fn=hps.loss_fn, hps=hps)
+            out_batch, loss, _metrics = model(x, **forw_kwargs)
+            
+            save_wav(f'{logger.logdir}/batch_{i}', out_batch, hps.sr) # take the batch
+                
+
+def run(hps="teeny", port=29500, **kwargs):
+    '''Do inference over a dataset using a trained model and save the results in the specified directory.
+    '''
     from jukebox.utils.dist_utils import setup_dist_from_mpi
     rank, local_rank, device = setup_dist_from_mpi(port=port)
     hps = setup_hparams(hps, kwargs)
@@ -20,19 +36,13 @@ def run(hps="vqvae", port=29500, **kwargs):
     
     # Setup model
     vqvae = load_checkpoint(hps.path_to_checkpoint)
-    distributed_vqvae = get_ddp(vqvae, hps)
-    ema = get_ema(vqvae, hps)
+    vqvae.to(device)
     
     logger, metrics = init_logging(hps, local_rank, rank)
     logger.iters = vqvae.step
     
-    if ema: ema.swap()
-    test_metrics = evaluate(distributed_vqvae, vqvae, logger, metrics, data_processor, hps)
-    if rank == 0:
-        print('Ema',' '.join([f'{key}: {val:0.4f}' for key,val in test_metrics.items()]))
-    dist.barrier()
-    if ema: ema.swap()
-    dist.barrier()
+    inference(vqvae, hps, data_processor, logger)
+
 
 if __name__ == '__main__':
     fire.Fire(run)
