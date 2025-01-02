359b901f7e192f6ef95fbbb275659ec593e3f887
Mattia Paolacci <mat.paolacci@gmail.com>
2025-01-02 20:39:26 +0100

    fix

--- jukebox/jukebox/inference.py
+++ jukebox/jukebox/inference.py
@@ -57,7 +57,8 @@ def encode_and_save(model: VQVAE, hps: Hyperparams, data_processor: DataProcesso
                 track_idx = data_processor.dataset.get_song_index(i)
                 track_data = t.tensor([])
             x_l = x_l[hps.use_level].cpu()
-            track_data = t.cat((track_data, x_l.reshape(int(x_l.shape[0]*x_l.shape[1]))), dim=0)
+            x_l = x_l.view(int(x_l.shape[0]*x_l.shape[1]))
+            track_data = t.cat((track_data, x_l), dim=0)
             if i == len(data_processor.test_loader) - 1:
                 save_embeddings(track_data, f'{logger.logdir}/encoded_data/track_{track_idx}')
 
