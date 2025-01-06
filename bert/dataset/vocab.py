6f182e91518db58c602350fbe5603be0f3bc5b7d
Mattia Paolacci <mat.paolacci@gmail.com>
2025-01-06 16:29:14 +0100

    added comment

--- bert/dataset/vocab.py
+++ bert/dataset/vocab.py
@@ -38,7 +38,7 @@ class TorchVocab(object):
         counter = counter.copy()
         min_freq = max(min_freq, 1)
 
-        # itos is a list of tokens (i.e. normal words along with special tokens)
+        # itos is a list of tokens (i.e. normal words along with special tokens), that will be sorted by frquency
         self.itos = list(specials)
         
         # frequencies of special tokens are not counted when building vocabulary
