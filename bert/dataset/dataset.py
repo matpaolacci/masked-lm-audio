687caad71b91bb2b85e65e37f9cbfcbfe614d393
Mattia Paolacci <mat.paolacci@gmail.com>
2025-01-11 22:23:58 +0100

    Adjusted the dataset

--- bert/dataset/dataset.py
+++ bert/dataset/dataset.py
@@ -1,70 +1,68 @@
 from torch.utils.data import Dataset
-import tqdm
-import torch
-import random
-
+import tqdm, random, os
+import torch as t
+from vocab import WordVocab
 
 class BERTDataset(Dataset):
-    def __init__(self, corpus_path, vocab, seq_len, encoding="utf-8", corpus_lines=None, on_memory=True):
-        self.vocab = vocab
+    def __init__(self, path_to_data, vocab, seq_len):
+        '''
+            :param seq_len: model input sequence
+        '''
+        self.vocab: WordVocab = vocab
         self.seq_len = seq_len
-
-        self.on_memory = on_memory
-        self.corpus_lines = corpus_lines
-        self.corpus_path = corpus_path
-        self.encoding = encoding
-
-        with open(corpus_path, "r", encoding=encoding) as f:
-            if self.corpus_lines is None and not on_memory:
-                for _ in tqdm.tqdm(f, desc="Loading Dataset", total=corpus_lines):
-                    self.corpus_lines += 1
-
-            if on_memory:
-                self.lines = [line[:-1].split("\t")
-                              for line in tqdm.tqdm(f, desc="Loading Dataset", total=corpus_lines)]
-                self.corpus_lines = len(self.lines)
-
-        if not on_memory:
-            self.file = open(corpus_path, "r", encoding=encoding)
-            self.random_file = open(corpus_path, "r", encoding=encoding)
-
-            for _ in range(random.randint(self.corpus_lines if self.corpus_lines < 1000 else 1000)):
-                self.random_file.__next__()
+        self._load_filenames(path_to_data)
+        self._load_sequence()
+        
+    def _load_filenames(self, path_to_data):
+        self.filenames = []
+        self.total_element = 0
+        for filename in os.listdir(path_to_data):
+            file_path = os.path.join(path_to_data, filename)
+            if os.path.isfile(file_path):
+                self.filenames.append(file_path)
+                self.total_element += 1
+                
+    def _load_sequence(self):
+        '''TODO: it takes only the first file'''
+        file_embedding_sequence: t.Tensor = t.load(self.filenames[0], map_location=t.device("cpu"))
+        embds_to_remove = file_embedding_sequence.shape[0] % self.seq_len
+        file_embedding_sequence = file_embedding_sequence[:file_embedding_sequence-embds_to_remove]
+        self.batches = file_embedding_sequence.view(file_embedding_sequence//self.seq_len, self.seq_len)
 
     def __len__(self):
-        return self.corpus_lines
+        return self.total_element
 
     def __getitem__(self, item):
-        t1, t2, is_next_label = self.random_sent(item)
-        t1_random, t1_label = self.random_word(t1)
-        t2_random, t2_label = self.random_word(t2)
+        input_sequence = self.batches[item]
+        
+        t1_random, t1_label = self.random_embedding(input_sequence)
 
         # [CLS] tag = SOS tag, [SEP] tag = EOS tag
         t1 = [self.vocab.sos_index] + t1_random + [self.vocab.eos_index]
-        t2 = t2_random + [self.vocab.eos_index]
 
         t1_label = [self.vocab.pad_index] + t1_label + [self.vocab.pad_index]
-        t2_label = t2_label + [self.vocab.pad_index]
 
-        segment_label = ([1 for _ in range(len(t1))] + [2 for _ in range(len(t2))])[:self.seq_len]
-        bert_input = (t1 + t2)[:self.seq_len]
-        bert_label = (t1_label + t2_label)[:self.seq_len]
+        bert_input = t1
+        bert_label = t1_label
 
-        padding = [self.vocab.pad_index for _ in range(self.seq_len - len(bert_input))]
-        bert_input.extend(padding), bert_label.extend(padding), segment_label.extend(padding)
+        #padding = [self.vocab.pad_index for _ in range(self.seq_len - len(bert_input))]
+        #bert_input.extend(padding), bert_label.extend(padding), segment_label.extend(padding)
 
         output = {"bert_input": bert_input,
-                  "bert_label": bert_label,
-                  "segment_label": segment_label,
-                  "is_next": is_next_label}
+                  "bert_label": bert_label}
 
-        return {key: torch.tensor(value) for key, value in output.items()}
+        return {key: t.tensor(value) for key, value in output.items()}
 
-    def random_word(self, sentence):
-        tokens = sentence.split() # list of words, e.g., ["I", "am", "a", "computer", "scientist"] 
+    def random_embedding(self, sequence):
+        '''
+            It takes a sequence of embeddings (indices) and replaces 15% of them by following the masking procedure in BERT.
+            :return : the output_label
+            
+            :param sequence: list of the audio embedding indexes
+        '''
         output_label = [] # it will filled with the true label
 
-        for i, token in enumerate(tokens):
+        for i, token in enumerate(sequence):
             prob = random.random()
             if prob < 0.15:
                 # The probability is rescaled by dividing it by 0.15 (bringing it back to a range between 0 and 1)
@@ -72,59 +70,24 @@ class BERTDataset(Dataset):
 
                 # 80% randomly change token to mask token
                 if prob < 0.8:
-                    tokens[i] = self.vocab.mask_index
+                    sequence[i] = self.vocab.mask_index
 
                 # 10% randomly change token to random token
                 elif prob < 0.9:
-                    tokens[i] = random.randrange(len(self.vocab))
+                    sequence[i] = random.randrange(len(self.vocab)) 
 
                 # 10% randomly change token to current token
                 else:
                     # stoi works like this: given a word, it returns its index as found in itos
-                    tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
+                    sequence[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
 
                 output_label.append(self.vocab.stoi.get(token, self.vocab.unk_index))
 
             # Leave 85% of the tokens as they are, they will not concur in the loss calculation
+            #   We label them with index=0
             else:
-                tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
+                sequence[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
                 # Indexes 0 will be ignored when we will calculate the loss
                 output_label.append(0)
 
-        # tokens will be like [12,34,1, 3, 1 , ...]
-        return tokens, output_label
-
-    def random_sent(self, index):
-        t1, t2 = self.get_corpus_line(index)
-
-        # output_text, label(isNotNext:0, isNext:1)
-        if random.random() > 0.5:
-            return t1, t2, 1
-        else:
-            return t1, self.get_random_line(), 0
-
-    def get_corpus_line(self, item):
-        if self.on_memory:
-            return self.lines[item][0], self.lines[item][1]
-        else:
-            line = self.file.__next__()
-            if line is None:
-                self.file.close()
-                self.file = open(self.corpus_path, "r", encoding=self.encoding)
-                line = self.file.__next__()
-
-            t1, t2 = line[:-1].split("\t")
-            return t1, t2
-
-    def get_random_line(self):
-        if self.on_memory:
-            return self.lines[random.randrange(len(self.lines))][1]
-
-        line = self.file.__next__()
-        if line is None:
-            self.file.close()
-            self.file = open(self.corpus_path, "r", encoding=self.encoding)
-            for _ in range(random.randint(self.corpus_lines if self.corpus_lines < 1000 else 1000)):
-                self.random_file.__next__()
-            line = self.random_file.__next__()
-        return line[:-1].split("\t")[1]
+        return output_label
